bd20331cb76d740d9fa554ce55926154629e2b79
Chukwukodinaka Ugochukwu <benjamin_kodi@outlook.com>
2025-01-26 21:55:20 +0100

    Made vital changes to the DB

--- Migrations/20250123152220_initializethelistwhentheobjectiscreated.Designer.cs
+++ Migrations/20250123152220_initializethelistwhentheobjectiscreated.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using BloggingApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BloggingApp.Migrations
 {
     [DbContext(typeof(ApplicationDbcontext))]
-    partial class ApplicationDbcontextModelSnapshot : ModelSnapshot
+    [Migration("20250123152220_initializethelistwhentheobjectiscreated")]
+    partial class initializethelistwhentheobjectiscreated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
