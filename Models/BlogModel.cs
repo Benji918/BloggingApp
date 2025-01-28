09c9bd2cbcc1bce600a51091ea2cb8a4ad5a8974
Chukwukodinaka Ugochukwu <kodiugos@gmail.com>
2025-01-28 02:43:32 +0100

    changed data annotations

--- Models/BlogModel.cs
+++ Models/BlogModel.cs
@@ -10,7 +10,8 @@ namespace BloggingApp.Models
         [Required]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Title is Required!")]
+        [StringLength(100, ErrorMessage = "Title must be less than 100 characters")]
         public string Title { get; set; }
 
         [Required]
